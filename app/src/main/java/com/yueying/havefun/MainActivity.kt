1b283f5e219039093a0acfbf70dd6d5e23c7effa
cnie <cnie@microstrategy.com>
2024-02-19 16:19:36 +0800

    enableEdgeToEdge display

--- app/src/main/java/com/yueying/havefun/MainActivity.kt
+++ app/src/main/java/com/yueying/havefun/MainActivity.kt
@@ -3,7 +3,9 @@ package com.yueying.havefun
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,11 +17,14 @@ import com.yueying.havefun.ui.theme.HaveFunTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             HaveFunTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .safeDrawingPadding(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
